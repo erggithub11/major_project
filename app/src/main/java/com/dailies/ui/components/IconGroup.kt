587a406430e0b223958ba23cc78dd0141066a657
erggithub11 <erg11@aber.ac.uk>
2024-02-26 08:22:29 +0000

    Adding in Icongroup for navigation bar development

---
+++ app/src/main/java/com/dailies/ui/components/IconGroup.kt
@@ -0,0 +1,9 @@
+package com.dailies.ui.components
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+class IconGroup (
+    val filledIcon: ImageVector,
+    val outlineIcon: ImageVector,
+    val label: String
+)
