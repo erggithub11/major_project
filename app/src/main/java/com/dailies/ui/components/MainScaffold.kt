6888c8233271bf72d066402a475a8c9a9dc998dd
erggithub11 <erg11@aber.ac.uk>
2024-02-26 07:35:12 +0000

    Adding a main scaffold class

--- app/src/main/java/com/dailies/ui/components/MainScaffold.kt
+++ app/src/main/java/com/dailies/ui/components/MainScaffold.kt
@@ -1,2 +1,22 @@
 package com.dailies.ui.components
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun MainScaffold (
+    navController: NavHostController,
+    floatingActionButton: @Composable () -> Unit = { },
+    snackbarContent: @Composable (SnackbarData) -> Unit = {},
+    coroutineScope: CoroutineScope,
+    snackbarHostState: SnackbarHostState? = null,
+    pageContent: @Composable (innerPadding: PaddingValues) -> Unit = {},
+
+
+    ){
+
+}
