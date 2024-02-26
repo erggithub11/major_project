ea07baa8205774729025cd27a0ae74dcd5839627
erggithub11 <erg11@aber.ac.uk>
2024-02-26 07:21:11 +0000

    Update setting screen

--- app/src/main/java/com/dailies/ui/settings/SettingScreen.kt
+++ app/src/main/java/com/dailies/ui/settings/SettingScreen.kt
@@ -1,2 +1,49 @@
 package com.dailies.ui.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun SettingScreenTopLevel(
+    navController: NavHostController
+){
+
+    SettingScreen(
+        navController = navController
+    )
+}
+
+@Composable
+fun SettingScreen(modifier: Modifier = Modifier,
+                navController: NavHostController
+){
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(10.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+    ) {
+
+        Text(text = "placeholder for Settings",
+            fontSize = 50.sp,
+            modifier = Modifier.padding(start = 8.dp))
+
+
+    }
+}
