1c5c8bbc68c7510ae1aa19ee3e316f74c5957350
erggithub11 <erg11@aber.ac.uk>
2024-03-10 09:40:14 +0000

    made Dailies into a entity

--- app/src/main/java/com/dailies/model/Dailies.kt
+++ app/src/main/java/com/dailies/model/Dailies.kt
@@ -1,8 +1,12 @@
 package com.dailies.model
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.time.DayOfWeek
 
+@Entity(tableName ="dailies")
 data class Dailies(
+    @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
     var name: String,
     var description: String,
